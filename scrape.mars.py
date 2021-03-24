756061ab8cfe5fde97e7b59c43eb9f3baff1b927
billye <billyemerritts@gmail.com>
2021-03-23 23:58:25 -0400

    changes

--- scrape.mars.py
+++ scrape.mars.py
@@ -1,12 +1,47 @@
-import splinter 
+import splinter import Browser
 import beautifulsoup
 import pandas as pd 
-import request 
+import requests
 import time
 import pymongo
+import json
+import numpy as np
 
 
-#Database Setup
-client = pymongo.mongoClient('https://mars.nasa.gov/news/')
+def init_browser():
+    #splinter
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    return browers('chrome', **executable_path, headless=False)
+
+def scrape():
+    #mars news titles
+    browser = init_browser
+    mars_collection = {}
+
+    #Database Setup
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+    time.sleep(1)
+
+html = browser.html
+soup = beautifulsoup(html, 'html.parser')
+
+mars_collection["news_title"] = soup.find('div', class_="content_title").get_text()
+mars_collection["news_snip"] = soup.find('div', class_="rollover_description_inner").get_text()
+
+#mars images
+url_feature_image = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+browser.visit(url_feature_image)
+response = browser.html
+soup2 = beautifulsoup(response, 'html.parser')
+images = soup2.find_all('a', class_="fancybox")
+pic_source = []
+
+    for image in images:
+        picture = image['data-fancybox-href']
+        pic_source.apped(picture)
+
+
+mars_collection["featured_image_url"] = ""   
 
 
