cec8e77548537516a2646f7b7b05c263bd8ad2ec
billye <billyemerritts@gmail.com>
2021-03-23 23:19:04 -0400

    changes

---
+++ scrape.mars.py
@@ -0,0 +1,12 @@
+import splinter 
+import beautifulsoup
+import pandas as pd 
+import request 
+import time
+import pymongo
+
+
+#Database Setup
+client = pymongo.mongoClient('https://mars.nasa.gov/news/')
+
+
